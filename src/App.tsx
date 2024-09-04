f7e1fe0b7f6954627a2685874be9ee1c3924f37d
icetech233 <hanbin@jiuxiniot.com>
2024-09-04 16:57:23 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import './App.css'
 import { Button } from "./components/ui/button"
 
 export default function App() {
-/*
- * 变量声明
- */
+  /*
+  * 变量声明
+  */
   const [count, setCount] = useState(0)
 
   return (
@@ -20,13 +20,11 @@ export default function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-
       </div>
-
 
       <h1>Vite + React</h1>
       <div className="p-8">
-        <Button className='min-w-32' onClick={() => setCount((count) => count + 1)}>
+        <Button className='min-w-32' onClick={() => setCount(aa => aa + 1)}>
           count is {count}
         </Button>
         <p>
@@ -35,16 +33,15 @@ export default function App() {
       </div>
 
       <div className='flex'>
-            {/* logo */}
-            <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
-              <circle cx="12" cy="12" r="11" />
-              <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
-            </svg>
-            <p className="text-rose-900  ">
-              Click on the Vite and React logos to learn more
-            </p>
+        {/* logo */}
+        <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+          <circle cx="12" cy="12" r="11" />
+          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+        </svg>
+        <p className="text-rose-900 mx-1.5">
+          Click on the Vite and React logos to learn more
+        </p>
       </div>
-
     </>
   )
 }
