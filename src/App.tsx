e8cce3c4301512a21740b3db074a4dbd5c98e61b
icetech233 <hanbin@jiuxiniot.com>
2024-09-04 10:45:42 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import './App.css'
 import { Button } from "./components/ui/button"
 
 export default function App() {
-
+/*
+ * 变量声明
+ */
   const [count, setCount] = useState(0)
 
   return (
@@ -19,8 +21,10 @@ export default function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="p-8">
         <Button className='min-w-32' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
@@ -28,7 +32,13 @@ export default function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+
+      {/* logo */}
+      <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+        <circle cx="12" cy="12" r="11" />
+        <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+      </svg>
+      <p className="text-rose-900">
         Click on the Vite and React logos to learn more
       </p>
     </>
